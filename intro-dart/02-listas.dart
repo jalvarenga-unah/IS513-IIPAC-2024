af23976e3af42828c70924150fadb4045431781a
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-04 18:42:58 -0600

    Uso de map y where

--- intro-dart/02-listas.dart
+++ intro-dart/02-listas.dart
@@ -20,8 +20,42 @@ void main() {
 //     print(postre);
   }
 
+  // final copia_postres = postres; //❌ no es una copia
+
+  //? Desestructuración, con el operador spread
+  final copia_postres = [...postres]; // ✅ si es una copia
+
+  copia_postres.remove('galletas');
+
+  print(postres);
+  print(copia_postres);
+
   //callback / función anonima
-  postres.forEach((String? postre) {
-    print(postre);
-  });
+  // postres.forEach((postre) {
+  //   print(postre);
+  // });
+
+  final numeros = [1, 2, 3, 4, 5];
+
+  //? Otra forma de hacer copias
+  final supuestos_postres = postres.map((postre) {
+    // postre = 'brocoli';
+
+    return postre;
+  }).toList();
+
+  final pares = numeros.map((valor) {
+    if (valor % 2 != 0) return valor * 2;
+    return valor;
+  }).toList();
+
+  final impares = numeros.where((valor) {
+    return (valor % 2 != 0);
+  }).toList();
+
+  print(numeros);
+  print(pares);
+  print(impares);
+
+  print(supuestos_postres);
 }
