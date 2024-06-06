e6dd279a25ba286c3bdb9cbea5fbe776ded20a15
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-05 18:23:29 -0600

    Modificando los Map

--- intro-dart/03-diccionario.dart
+++ intro-dart/03-diccionario.dart
@@ -3,7 +3,8 @@ void main() {
     'nombre': 'Juan',
     'edad': 30,
     'comidas_favoritas': ['Pizza', 'Sopas'],
-    'es_mayor': true
+    'es_mayor': true,
+    'geo': {'lat': 14.123123, 'lng': -87.123123}
   };
 
   final roles = {
@@ -15,6 +16,15 @@ void main() {
 
   persona['nombre'] = 'enrique';
   persona['direccion'] = "UNAH-VS";
+
+  persona.remove('geo'); //eliminar una propiedad
+
+  // permite agregar mas propiedades al Map
+  persona.addAll({
+    'telefono': '123456789',
+    'edad': 31,
+    'es_mayor': false,
+  });
 
   print(persona);
 }
