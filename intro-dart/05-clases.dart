b91b1a5cac0dd412f7f54032ce6b527a201b4a53
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-10 19:02:10 -0600

    Constructores y getters y setters

--- intro-dart/05-clases.dart
+++ intro-dart/05-clases.dart
@@ -2,10 +2,29 @@ import 'Mascota.dart';
 
 void main() {
   final mascota1 = Mascota(nombre: 'Apolo', raza: 'Terrier');
+  final mascota2 = Mascota.soloNombre(nombre: 'Polar');
+  final mascota3 = Mascota.conEdad(2);
 
-  print(mascota1.nombre);
-  print(mascota1.raza);
-  print(mascota1.toString());
+  final datos = {'nombre': 'Pulgas', 'raza': 'Aguacatero'};
+
+  final mascota4 = Mascota.fromMap(datos);
+
+  print(mascota4.toString());
+
+  // mascota1.setEdad(2);
+
+  // try {
+  //   mascota1.edad = 2;
+  // } catch (error) {
+  //   print(error);
+  // }
+
+  // print(mascota1.nombre);
+  // print(mascota1.raza);
+  // // print(mascota1.getEdad());
+  // print(mascota1.edad);
+
+  // print(mascota1.toString());
 
   // print(mascota1)
 }
--- intro-dart/Mascota.dart
+++ intro-dart/Mascota.dart
@@ -3,9 +3,9 @@ class Mascota {
 
   // "late" le indica a clase, que la propiedad, tendrá un valor en el futuro
   // late String nombre;
-  final String nombre;
-  final String raza;
-  int _edad = 0;
+  late String nombre;
+  late String raza;
+  int _edad = 0; // Privada
 
   //constructor
 
@@ -21,17 +21,41 @@ class Mascota {
     // this._edad = 0,
   });
 
-  //TODO: los constructor factory
+  //* constructor factory
+  factory Mascota.fromMap(Map<String, dynamic> map) {
+    return Mascota(nombre: map['nombre'], raza: map['raza']);
+  }
 
-  //TODO: conscturcotre con nombre
+  //* constructores con nombre
+  Mascota.soloNombre({required this.nombre}); // ?????
+  Mascota.conEdad(this._edad);
 
 //sobreescribir el metodo
   @override
   String toString() {
+    // return super.toString();
     return 'Hola soy ${this.nombre} soy un $raza y tengo $_edad años';
   }
 
   // comportamientos / metodos
 
-  //TODO: crear metodos getters y setters para "_edad"
+  // int getEdad() {
+  //   return this._edad;
+  // }
+
+  // void setEdad(int edad) {
+  //   this._edad = edad;
+  // }
+
+  // lambda function
+  int get edad => this._edad; // getter
+
+  set edad(int edad) {
+    if (edad <= 0) {
+      // Generar una excepcion (Error)
+      throw Exception('La edad no es válida');
+    }
+
+    this._edad = edad;
+  } // setter
 }
