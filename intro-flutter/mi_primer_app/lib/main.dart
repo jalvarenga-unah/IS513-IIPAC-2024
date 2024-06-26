7ec58b15aff541d4b2484f8e8da168fe9f85c3b2
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-25 18:08:43 -0600

    uso de Columns y Row

--- intro-flutter/mi_primer_app/lib/main.dart
+++ intro-flutter/mi_primer_app/lib/main.dart
@@ -16,15 +16,43 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           // centerTitle: false,
-          backgroundColor: Colors.indigo[300],
+          backgroundColor: Colors.indigo[800],
           // backgroundColor: Color(0xff009688),
-          title: const Align(
-            alignment: Alignment.center,
-            child: Text('Mi primer app'),
+          title: Text(
+            'Mi primer app',
+            style: TextStyle(color: Colors.indigo[100]),
           ),
         ),
-        body: const Center(
-          child: Text('Hola mundo'),
+        drawer: Drawer(),
+        body: Center(
+          child: Container(
+            // color: Colors.red,
+            width: MediaQuery.of(context).size.width,
+            child: const Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('oli'),
+                Text('oli'),
+                Text('Hizo tantas veces clic'),
+                Text(
+                  '1',
+                ),
+              ],
+            ),
+          ),
+        ),
+        // floatingActionButtonLocation:
+        //     FloatingActionButtonLocation.miniCenterTop,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('oliii');
+          },
+          child: Icon(
+            Icons.plus_one,
+            color: Colors.pink[800],
+          ),
         ),
       ),
     );
